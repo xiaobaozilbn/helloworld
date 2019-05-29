a299f51838c8ce7cb876639bcb7f2f1f2e151806
Ahri02 <51103601+Ahri02@users.noreply.github.com>
2019-05-29 09:15:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ import machine
 
 print('***************adding from pythonA****************')
 print('***************this is a demo****************')
-print('***************master adding****************')
+print('***************2233****************')
