22c2dc59c15d1943ffb2dfca79f84ee58d534701
xiaobaozilbn <51103741+xiaobaozilbn@users.noreply.github.com>
2019-05-29 08:27:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ import machine
 print('***************adding from pythonA****************')
 print('***************this is a demo****************')
 print('***************master adding****************')
+print('***************adding from pythonD branch****************')
+print('***************adding from master branch****************')
